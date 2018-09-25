3b7c31b4045266d51b4d101a015a40c9d3c3880e
coke <dujun.sc@outlook.com>
2018-09-25 23:57:37 +0800

    foot 改变到edit界面

--- src/views/edit/edit.vue
+++ src/views/edit/edit.vue
@@ -3,16 +3,22 @@
 		<h1>这里是标题</h1>
 		<div class="line"></div>
 		<el-button class="primary">添加问题</el-button>
+		<div class="question-main"></div>
+		<foot></foot>
 	</div>
 </template>
 
 <script>
+import foot from "./foot";
 export default {
 	name: 'new',
 	data: function() {
 		return {
 
 		}
+	},
+	components: {
+		foot,
 	}
 }
 </script>
--- src/views/profile/profile.vue
+++ src/views/profile/profile.vue
@@ -1,16 +1,13 @@
 <template>
 	<div class="profile" >
 		<h1>profile</h1>
-		<foot></foot>
 	</div>
 </template>
 
 <script>
-import foot from './foot'
 export default {
 	name: 'profile',
 	components: {
-		foot,
 	}
 }
 </script>
