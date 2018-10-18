7b35726b2c26b397dfeff83ef788898ae7688265
coke <dujun.sc@outlook.com>
2018-10-19 01:19:45 +0800

    高斯模糊

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
     <img src="./assets/logo.png">
     <router-view></router-view>
     <router-view name="a"></router-view>
+	<main>
+		<blockquote>
+			“The only way to get rid of a temptation[…]”
+			<footer>—
+				<cite>
+					Oscar Wilde,
+					The Picture of Dorian Gray
+				</cite>
+			</footer>
+		</blockquote>
+	</main>
   </div>
 </template>
 
@@ -25,5 +36,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 30px;
+}
+main {
+	// background: hsla(0,0%,100%, 0.3);
+	position: relative;
+	    position: absolute;
+    top: 100px;
+	left: 100px;
+	overflow: hidden;
+}
+
+
+main::before {
+    content: '';
+	position: absolute;
+
+    top: 0; right: 0; bottom: 0; left: 0;
+	background: rgba(255,0,0,.5); /* for debugging */
+	filter: blur(20px);
+	opacity: 0.5;
+	z-index: -1;
+	// margin: -30px;
+
 }
 </style>
--- src/components/surveyNav.vue
+++ src/components/surveyNav.vue
@@ -41,6 +41,16 @@ export default {
 <style>
 .navigator {
 	/* background: orange; */
+	position: relative;
+}
+.navigator::before {
+    content: '';
+    position: absolute;
+    top: 0; right: 0; bottom: 0; left: 0;
+    z-index: -1;
+    -webkit-filter: blur(20px);
+    filter: blur(20px);
+    background: rgba(255,0,0,.5);
 }
 </style>
 
